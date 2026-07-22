bac88c9f509901ea3c0f92798bb0e49cf3e1a59f
Anmol Sharma <Anmol20102005@gmail.com>
2026-07-22 15:09:51 +0530

    updated the grpc server

--- internal/tablet/server/server.go
+++ internal/tablet/server/server.go
@@ -2,18 +2,35 @@ package server
 
 import (
 	"google.golang.org/grpc"
+	"context"
 	"fmt"
 	"net"
+	"github.com/Anmol202005/VScale/internal/tablet/executor"
+	"github.com/Anmol202005/VScale/internal/tablet/pool"
+	pb "github.com/Anmol202005/VScale/proto/tablet"
 )
 type Server struct {
 	grpcServer *grpc.Server
 	port int
+
+	pool *pool.Pool
 }
 
-func NewServer(port int) *Server {
+func NewServer(port int, connString string) *Server {
+	grpcServer := grpc.NewServer()
+	p, err := pool.NewPool(context.Background(), connString)
+	if err != nil {
+		panic(fmt.Sprintf("failed to create pool: %v", err))
+	}
+
+	handler := NewTabletHandler()
+	handler.executor = executor.NewExecutor(p)
+
+	pb.RegisterTabletServiceServer(grpcServer, handler)
 	return &Server{
 		grpcServer: grpc.NewServer(),
 		port: port,
+		pool: p,
 	}
 }
 
