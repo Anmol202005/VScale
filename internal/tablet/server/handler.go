5100bcec909db8cfc2fac6e9b4d3530dd3ff5820
Anmol Sharma <Anmol20102005@gmail.com>
2026-07-22 13:01:56 +0530

    implemented server handler and proto

--- internal/tablet/server/handler.go
+++ internal/tablet/server/handler.go
@@ -1,1 +1,35 @@
 package server
+
+import (
+	"context"
+
+	pb "github.com/Anmol202005/VScale/proto/tablet"
+)
+
+type TabletHandler struct {
+	pb.UnimplementedTabletServiceServer
+}
+
+func NewTabletHandler() *TabletHandler {
+	return &TabletHandler{}
+}
+
+func (h *TabletHandler) Execute(
+	ctx context.Context,
+	req *pb.QueryRequest,
+) (*pb.QueryResponse, error) {
+
+	return &pb.QueryResponse{
+		Message: "Query received: " + req.Sql,
+	}, nil
+}
+
+func (h *TabletHandler) Health(
+	ctx context.Context,
+	req *pb.HealthRequest,
+) (*pb.HealthResponse, error) {
+
+	return &pb.HealthResponse{
+		Healthy: true,
+	}, nil
+}
